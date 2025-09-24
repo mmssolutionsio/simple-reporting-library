5cb44fb5d2d8fe90367119263cbfd8d96cc00a09
claudio.schwarz <claudio.schwarz@neidhartschoen.ch>
2025-09-24 16:25:25 +0200

    update livingdoc components from project work

---
+++ livingdocs/040.Media/010.table/table.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TableResponsive from './table/responsive.vue'
+import TableDownload from './table/download.vue'
+
+const component = ref<HTMLDivElement>()
+</script>
+
+<template>
+  <div ref="component">
+    <TableResponsive :component="component" />
+    <slot/>
+  </div>
+</template>
